1075a3150f48d825958b63001f39c016c775cb5a
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-28 16:58:11 -0400

    added comments explaining api

--- flask/app.py
+++ flask/app.py
@@ -4,10 +4,12 @@ import json
 app = Flask(__name__)
 
 
-
+#import data here and refresh (?) when a get /post is received? what is best practice?
 
 @app.route('/', methods=['GET'])
 def query_records():
+    #this is for query parameters such as ipaddr/name=xcksdee
+    #this part of the code looks at the records and returns json if the record is availible. 
     name = request.args.get('name')
     print(name)
     with open('/tmp/data.txt', 'r') as f:
@@ -18,7 +20,11 @@ def query_records():
                 return jsonify(record)
         return jsonify({'error': 'data not found'})
 
-@app.route('/api', methods=['PUT'])
+@app.route('/', methods=['PUT'])
+#this part of the code if it is a put request, will create new records. 
+
+#WE NEED TO IMPORT OUR DATA ON SERVER RUN
+
 def create_record():
     record = json.loads(request.data)
     with open('/tmp/data.txt', 'r') as f:

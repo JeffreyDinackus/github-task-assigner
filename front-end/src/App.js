978b184fc59a47c82936b3754b515b3fbe9684b3
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-28 18:09:59 -0400

    building react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import navbar from './components/navbar/navbar.jsx'
 
-function App() {
+
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+      <navbar />
+
+
+
+
     </div>
   );
 }

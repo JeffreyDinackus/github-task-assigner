6840343dc0b33d3cedb4d2a909bbcfd2d81d1118
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-28 16:54:43 -0400

    added basic api

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask,jsonify, request
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
+
+@app.route('/', methods=['GET'])
+def query_records():
+    name = request.args.get('name')
+    print(name)
+    with open('/tmp/data.txt', 'r') as f:
+        data = f.read()
+        records = json.loads(data)
+        for record in records:
+            if record['name'] == name:
+                return jsonify(record)
+        return jsonify({'error': 'data not found'})
+
+@app.route('/api', methods=['PUT'])
+def create_record():
+    record = json.loads(request.data)
+    with open('/tmp/data.txt', 'r') as f:
+        data = f.read()
+    if not data:
+        records = [record]
+    else:
+        records = json.loads(data)
+        records.append(record)
+    with open('/tmp/data.txt', 'w') as f:
+        f.write(json.dumps(records, indent=2))
+    return jsonify(record)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
